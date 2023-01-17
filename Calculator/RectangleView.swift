5a6d73f1579d3d7b23913361a272c1c4dc5a4a9d
Tom <twu@lcs.on.ca>
2023-01-17 11:38:15 -0500

    rectangleView

---
+++ Calculator/RectangleView.swift
@@ -0,0 +1,20 @@
+//
+//  RectangleView.swift
+//  Calculator
+//
+//  Created by Tom Wu on 2023-01-17.
+//
+
+import SwiftUI
+
+struct RectangleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RectangleView_Previews: PreviewProvider {
+    static var previews: some View {
+        RectangleView()
+    }
+}
