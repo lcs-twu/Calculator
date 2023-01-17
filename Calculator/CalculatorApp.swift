d5c15c0a2b4f58bf0046fdf64863221b1f0cbc52
Tom <twu@lcs.on.ca>
2023-01-17 11:34:35 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Tom Wu on 2023-01-17.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
